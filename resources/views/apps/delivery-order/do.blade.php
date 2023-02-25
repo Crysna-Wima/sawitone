e256f7cc3f95bb6abff1bfbfa3bce7c1564ce3ac
Izzudin Fasya <izzudinfasya@gmail.com>
2023-02-25 19:20:35 +0700

    fess

--- resources/views/apps/delivery-order/do.blade.php
+++ resources/views/apps/delivery-order/do.blade.php
@@ -667,7 +667,7 @@
             },
             columnDefs: [{
                 className: 'text-center',
-                targets: [0, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+                targets: [0, 3, 4, 5, 6, 7, 8, 9, 10, 12]
             }, ],
             columns: [{
                     data: 'DT_RowIndex',
