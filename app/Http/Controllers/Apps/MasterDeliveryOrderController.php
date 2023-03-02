334c9b52a9c8e65df6b8a004481850127b76eca7
aladino24 <aladinozulmar@gmail.com>
2023-03-02 07:54:53 +0700

    insert invmst

--- app/Http/Controllers/Apps/MasterDeliveryOrderController.php
+++ app/Http/Controllers/Apps/MasterDeliveryOrderController.php
@@ -19,6 +19,7 @@ use App\Models\SoMaster;
 use App\Models\SoDetail;
 use App\Models\DoDetail;
 use App\Models\DoMaster;
+use App\Models\InvMaster;
 
 class MasterDeliveryOrderController extends Controller
 {
@@ -72,7 +73,80 @@ class MasterDeliveryOrderController extends Controller
     }
 
     public function publish(Request $request){
-        // create or update
-        
+       // validasi semua request
+    //    dd($request);
+         $validator = Validator::make($request->all(),[
+          'fc_dono' => 'required',
+          'fc_divisioncode' => 'required',
+          'fc_branch' => 'required',
+        //   'fn_dodateinputuser' => 'required',
+          'fn_dodetail' => 'required',
+          'fm_disctotal' => 'required',
+          'fm_netto' => 'required',
+          'fm_servpay' => 'required',
+          'fm_tax' => 'required',
+          'fm_brutto' => 'required',
+          'fd_inv_releasedate' => 'required',
+          'fn_inv_agingday' => 'required',
+          'fn_inv_agingdate' => 'required'
+         ],
+         [
+            // pesan validasi
+            'fc_dono' => 'Nomor DO tidak boleh kosong',
+            'fc_divisioncode' => 'Kode Divisi tidak boleh kosong',
+            // 'fn_dodateinputuser' => 'Tanggal DO tidak boleh kosong',
+            'fd_inv_releasedate' => 'Tanggal Terbit Invoice tidak boleh kosong',
+            'fn_inv_agingday' => 'Masa Invoice tidak boleh kosong',
+            'fn_inv_agingdate' => 'Tanggal Jatuh Tempo Invoice tidak boleh kosong',
+         ]); 
+
+         // apabila validasi tidak sesuai
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()]);
+            }
+
+        // create  data in InvMaster
+        $inv_mst = InvMaster::Create(
+            // ['fc_dono' => $request->fc_dono],
+            [
+                'fc_dono' => $request->fc_dono,
+                'fc_branch' => $request->fc_branch,
+                'fc_invno' => auth()->user()->fc_userid,
+                'fc_sono' => $request->fc_sono,
+                'fc_divisioncode' => $request->fc_divisioncode,
+                'fn_invdetail' => $request->fn_dodetail,
+                'fm_disctotal' => $request->fm_disctotal,
+                'fm_netto' => $request->fm_netto,
+                'fc_userid' => auth()->user()->fc_userid,
+                'fm_servpay' => $request->fm_servpay,
+                'fc_status' => 'T',
+                'fm_tax' => $request->fm_tax,
+                'fm_brutto' => $request->fm_brutto,
+                'fd_inv_releasedate' => $request->fd_inv_releasedate,
+                'fn_inv_agingday' => $request->fn_inv_agingday,
+                'fn_inv_agingdate' => $request->fn_inv_agingdate,
+                'fd_inv_releasedate' => $request->fd_inv_releasedate,
+            ]);
+
+        // update data fc_invstatus in DoMaster
+        $do_mst = DoMaster::where('fc_dono', $request->fc_dono)->update(['fc_invstatus' => 'Y']);
+
+         // // jika validasi sukses dan $do_master berhasil response 200
+         if ($inv_mst && $do_mst) {
+            return response()->json(
+                [
+                    'status' => 200,
+                    'message' => 'Invoice berhasil diterbitkan',
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'status' => 300,
+                    'message' => 'Invoice gagal diterbitkan'
+                ]
+            );
+        }
+
     }
 }
