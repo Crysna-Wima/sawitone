7fb714d1f3435f4a49fb4796de6e8f299e8dfc68
Izzudin Fasya <izzudinfasya@gmail.com>
2023-03-10 09:58:40 +0700

    detail PO

--- resources/views/apps/purchase-order/index.blade.php
+++ resources/views/apps/purchase-order/index.blade.php
@@ -193,7 +193,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-12 col-lg-8 place_detail">
+            <div class="col-12 col-md-12 col-lg-6 place_detail">
                 <div class="card">
                     <div class="card-body" style="padding-top: 30px!important;">
                         <form id="form_submit_custom" action="/apps/sales-order/detail/store-update" method="POST"
@@ -259,51 +259,58 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-12 col-lg-4 place_detail">
-                <div class="card">
-                    <div class="card-body" style="padding-top: 30px!important; height: 260px">
-                        <form id="form_submit" action="#" method="POST" autocomplete="off">
-                            @csrf
-                            @method('PUT')
-                            <div class="d-flex">
-                                <div class="flex-row-item">
-                                    <div class="d-flex" style="gap: 5px; white-space: pre">
-                                        <p class="text-secondary flex-row-item" style="font-size: medium">Item</p>
-                                        <p class="text-success flex-row-item text-right" style="font-size: medium"
-                                            id="count_item">0,00</p>
-                                    </div>
-                                    <div class="d-flex">
-                                        <p class="flex-row-item"></p>
-                                        <p class="flex-row-item text-right"></p>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Transport</label>
-                                        @if (empty($data->fc_sotransport))
-                                            <select class="form-control select2" name="" id="">
-                                                <option value="" selected disabled>- Pilih Transport -</option>
-                                                <option value="By Dexa">By Dexa</option>
-                                                <option value="By Paket">By Paket</option>
-                                                <option value="By Customer">By Customer</option>
-                                            </select>
-                                        @else
-                                            <select class="form-control select2" name="" id="">
-                                                <option value="#" selected disabled></option>
-                                                <option value="By Dexa">By Dexa</option>
-                                                <option value="By Paket">By Paket</option>
-                                                <option value="By Customer">By Customer</option>
-                                            </select>
-                                        @endif
-                                    </div>
-                                    <div class="d-flex">
-                                        <p class="flex-row-item"></p>
-                                        <p class="flex-row-item text-right"></p>
-                                    </div>
-                                    <div class="text-right">
-                                        <button class="btn btn-success">Save</button>
-                                    </div>
+            <div class="col-12 col-md-12 col-lg-6 place_detail">
+            <div class="card">
+                    <div class="card-header">
+                        <h4>Calculation</h4>
+                    </div>
+                    <div class="card-body" style="height: 190px">
+                        <div class="d-flex">
+                            <div class="flex-row-item" style="margin-right: 30px">
+                                <div class="d-flex" style="gap: 5px; white-space: pre">
+                                    <p class="text-secondary flex-row-item" style="font-size: medium">Item</p>
+                                    <p class="text-success flex-row-item text-right" style="font-size: medium" id="count_item">0,00</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="flex-row-item"></p>
+                                    <p class="flex-row-item text-right"></p>
+                                </div>
+                                <div class="d-flex" style="gap: 5px; white-space: pre">
+                                    <p class="text-secondary flex-row-item" style="font-size: medium">Disc. Total</p>
+                                    <p class="text-success flex-row-item text-right" style="font-size: medium" id="fm_so_disc">0,00</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="flex-row-item"></p>
+                                    <p class="flex-row-item text-right"></p>
+                                </div>
+                                <div class="d-flex" style="gap: 5px; white-space: pre">
+                                    <p class="text-secondary flex-row-item" style="font-size: medium">Total</p>
+                                    <p class="text-success flex-row-item text-right" style="font-size: medium" id="total_harga">0,00</p>
                                 </div>
                             </div>
-                        </form>
+                            <div class="flex-row-item">
+                                <div class="d-flex" style="gap: 5px; white-space: pre">
+                                    <p class="text-secondary flex-row-item" style="font-size: medium">Pelayanan</p>
+                                    <p class="text-success flex-row-item text-right" style="font-size: medium" id="fm_servpay">0,00</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="flex-row-item"></p>
+                                    <p class="flex-row-item text-right"></p>
+                                </div>
+                                <div class="d-flex" style="gap: 5px; white-space: pre" >
+                                    <p class="text-secondary flex-row-item" style="font-size: medium">Pajak</p>
+                                    <p class="text-success flex-row-item text-right" style="font-size: medium" id="fm_tax">0,00</p>
+                                </div>
+                                <div class="d-flex">
+                                    <p class="flex-row-item"></p>
+                                    <p class="flex-row-item text-right"></p>
+                                </div>
+                                <div class="d-flex" style="gap: 5px; white-space: pre">
+                                    <p class="text-secondary flex-row-item" style="font-weight: bold; font-size: medium">GRAND</p>
+                                    <p class="text-success flex-row-item text-right" style="font-weight: bold; font-size:medium" id="grand_total">Rp. 0,00</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -328,6 +335,79 @@
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-12 place_detail">
+                <div class="card">
+                    <div class="card-body" style="padding-top: 30px!important;">
+                        <form id="form_submit_custom" action="/apps/sales-order/detail/store-update" method="POST"
+                            autocomplete="off">
+                            <div class="row">
+                                <div class="col-12 col-md-12 col-lg-3">
+                                    <div class="form-group">
+                                        <label>Tanggal PO</label>
+                                        <div class="input-group" data-date-format="dd-mm-yyyy">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <i class="fas fa-calendar"></i>
+                                                </div>
+                                            </div>
+
+                                            <input type="text" id="" class="form-control datepicker"
+                                                name="" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6 col-lg-3">
+                                    <div class="form-group">
+                                            <label>Transport</label>
+                                            @if (empty($data->fc_sotransport))
+                                                <select class="form-control select2" name="" id="">
+                                                    <option value="" selected disabled>- Pilih Transport -</option>
+                                                    <option value="By Dexa">By Dexa</option>
+                                                    <option value="By Paket">By Paket</option>
+                                                    <option value="By Customer">By Customer</option>
+                                                </select>
+                                            @else
+                                                <select class="form-control select2" name="" id="">
+                                                    <option value="#" selected disabled></option>
+                                                    <option value="By Dexa">By Dexa</option>
+                                                    <option value="By Paket">By Paket</option>
+                                                    <option value="By Customer">By Customer</option>
+                                                </select>
+                                            @endif
+                                        </div>
+                                    </div>
+                                <div class="col-12 col-md-12 col-lg-3">
+                                    <div class="form-group">
+                                        <label>Perkiraan Penanganan</label>
+                                        <div class="input-group format-rp">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    Rp.
+                                                </div>
+                                            </div> 
+                                            <input type="text" id="" class="form-control"
+                                                name="" onkeyup="return onkeyupRupiah(this.id)" required>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6 col-lg-3">
+                                    <label>Alamat Tujuan</label>
+                                    <div class="form-group">
+                                        <input type="text" id="" class="form-control"
+                                                name="" required>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-12 col-lg-12 text-right">
+                                    <button class="btn btn-success">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="button text-right mb-4">
+                    <a href="#" class="btn btn-success">Submit</a>
                 </div>
             </div>
         </div>
