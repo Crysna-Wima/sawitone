7543ff2cf19a8923e25c08d1ed0293672de55ba0
Izzudin Fasya <izzudinfasya@gmail.com>
2023-03-11 21:37:47 +0700

    datatable detail PO

--- app/Http/Controllers/Apps/MasterPurchaseOrderController.php
+++ app/Http/Controllers/Apps/MasterPurchaseOrderController.php
@@ -16,52 +16,67 @@ use DB;
 
 use App\Models\PoMaster;
 use App\Models\PoDetail;
+use App\Models\RoMaster;
 use Yajra\DataTables\DataTables as DataTables;
 
 class MasterPurchaseOrderController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
         return view('apps.master-purchase-order.index');
     }
 
-    public function detail($fc_pono){
+    public function detail($fc_pono)
+    {
         session(['fc_pono_global' => $fc_pono]);
         $data['data'] = PoMaster::with('supplier')->where('fc_pono', $fc_pono)->first();
         return view('apps.master-purchase-order.detail', $data);
         // dd($data);
     }
-    
-    public function datatables(){
+
+    public function datatables()
+    {
         $data = PoMaster::with('supplier')->where('fc_branch', auth()->user()->fc_branch)->get();
 
         return DataTables::of($data)
-        ->addIndexColumn()
-        ->make(true);
+            ->addIndexColumn()
+            ->make(true);
         // dd($data);
     }
 
-    public function datatables_po_detail(){
+    public function datatables_po_detail()
+    {
         //  jika session fc_sono_global tidak sama dengan null
         if (session('fc_pono_global') != null) {
-           $fc_pono = session('fc_pono_global');
-       } else {
-           $pomst = PoMaster::where('fc_userid', auth()->user()->fc_userid)->first();
-           $fc_pono_pomst = $pomst->fc_pono;
-           $fc_pono = $fc_pono_pomst;
-       }
+            $fc_pono = session('fc_pono_global');
+        } else {
+            $pomst = PoMaster::where('fc_userid', auth()->user()->fc_userid)->first();
+            $fc_pono_pomst = $pomst->fc_pono;
+            $fc_pono = $fc_pono_pomst;
+        }
 
-       $data = PoDetail::with('branch', 'warehouse', 'stock', 'namepack')->where('fc_pono', $fc_pono)->get();
-       return DataTables::of($data)
-       ->addIndexColumn()
-       ->make(true);
-   }
+        $data = PoDetail::with('branch', 'warehouse', 'stock', 'namepack')->where('fc_pono', $fc_pono)->get();
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->make(true);
+    }
+
+    public function datatables_receiving_order()
+    {
+
+        $data = RoMaster::with('supplier')->where('fc_pono', session('fc_pono_global'))->get();
+
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->make(true);
+    }
 
     public function pdf($fc_pono)
     {
         session(['fc_pono_global' => $fc_pono]);
-        $data['po_mst']= PoMaster::with('supplier')->where('fc_pono', $fc_pono)->first();
-        $data['po_dtl']= PoDetail::where('fc_pono', $fc_pono)->get();
+        $data['po_mst'] = PoMaster::with('supplier')->where('fc_pono', $fc_pono)->first();
+        $data['po_dtl'] = PoDetail::with('branch', 'warehouse', 'stock', 'namepack')->where('fc_pono', $fc_pono)->get();
         $pdf = PDF::loadView('pdf.purchase-order', $data)->setPaper('a4');
         return $pdf->stream();
     }
--- app/Http/Controllers/Apps/ReceivingDetailOrderController.php
+++ app/Http/Controllers/Apps/ReceivingDetailOrderController.php
@@ -8,13 +8,13 @@ use Illuminate\Http\Request;
 class ReceivingDetailOrderController extends Controller
 {
     public function index(){
-        return view('apps.receiving-order.create-index');
+        return view('apps.receiving-order.create-detail');
     }
 
     
     public function create(){
         // $data = PoMaster::with('supplier')->where('fc_pono', auth()->user()->fc_userid)->first();
-        return view('apps.receiving-order.create-index');
+        return view('apps.receiving-order.create-detail');
         // $romst = RoMaster::where('fc_dono', auth()->user()->fc_userid)->first();
         // $data['romst'] = $romst;
         // dd($data);
