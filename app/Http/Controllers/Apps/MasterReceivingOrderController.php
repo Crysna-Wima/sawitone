8a4b250ceef07ce36d9bddd1b1ca5f71b3c955c0
Izzudin Fasya <izzudinfasya@gmail.com>
2023-03-13 23:44:06 +0700

    PDF RO di detail master PO

--- app/Http/Controllers/Apps/MasterReceivingOrderController.php
+++ app/Http/Controllers/Apps/MasterReceivingOrderController.php
@@ -29,7 +29,7 @@ class MasterReceivingOrderController extends Controller
     public function datatables()
     {
 
-        $data = RoMaster::with('pomst.supplier')->where('fc_pono', session('fc_pono_global'))->get();
+        $data = RoMaster::with('pomst.supplier')->where('fc_branch', auth()->user()->fc_branch)->get();
 
         return DataTables::of($data)
             ->addIndexColumn()
@@ -38,7 +38,7 @@ class MasterReceivingOrderController extends Controller
 
     public function pdf($fc_rono)
     {
-        session(['fc_pono_global' => $fc_rono]);
+        session(['fc_rono_global' => $fc_rono]);
         $data['ro_mst'] = RoMaster::with('pomst')->where('fc_rono', $fc_rono)->first();
         $data['ro_dtl'] = RoDetail::with('invstore.stock', 'romst')->where('fc_rono', $fc_rono)->get();
         $pdf = PDF::loadView('pdf.receiving-order', $data)->setPaper('a4');
