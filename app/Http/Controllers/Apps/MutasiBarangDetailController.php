60fbfbc5ddf4b6561e05fb7526b556da14fb2303
alfikiyarhaidar <alfikiyarhaidar@gmail.com>
2023-05-22 13:25:34 +0700

    Modifikasi Validator Mutasi DTL

--- app/Http/Controllers/Apps/MutasiBarangDetailController.php
+++ app/Http/Controllers/Apps/MutasiBarangDetailController.php
@@ -64,7 +64,7 @@ class MutasiBarangDetailController extends Controller
         $temp_detail = TempMutasiDetail::where('fc_mutationno', auth()->user()->fc_userid)->orderBy('fn_mutationrownum', 'DESC')->first();
 
         // jika ada TempSoDetail yang fc_stockcode == $request->fc_stockcode
-        $count_stockcode = TempMutasiDetail::where('fc_mutationno', auth()->user()->fc_userid)->where('fc_stockcode', $request->fc_stockcode)->get();
+        $count_stockcode = TempMutasiDetail::where('fc_mutationno', auth()->user()->fc_userid)->where('fc_barcode', $request->fc_barcode)->get();
 
 
         // jika ada fc_stockcode yang sama di $temppodtl
