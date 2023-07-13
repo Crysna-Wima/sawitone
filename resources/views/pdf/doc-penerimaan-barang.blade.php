25eeb8b8996578d79e20bf3ac722650601e819d7
Izzudin Fasya <izzudinfasya@gmail.com>
2023-07-13 13:53:19 +0700

    label

--- resources/views/pdf/doc-penerimaan-barang.blade.php
+++ resources/views/pdf/doc-penerimaan-barang.blade.php
@@ -29,22 +29,22 @@
             <tr>
                 <td style="width: 30%;"><b>Tgl Diterima</b></td>
                 <td style="width: 10; text-align:center" >:</td>
-                <td style="width: 45%">{{ \Carbon\Carbon::parse( $gr_mst->fd_arrivaldate )->isoFormat('D MMMM Y'); }}</td>
+                <td style="width: 35%">{{ \Carbon\Carbon::parse( $gr_mst->fd_arrivaldate )->isoFormat('D MMMM Y'); }}</td>
             </tr>   
             <tr>
                 <td style="width: 30%;"><b>No. Surat</b></td>
                 <td style="width: 10%; text-align:center">:</td>
-                <td style="width: 45%">{{ $gr_mst->fc_grno }}</td>
+                <td style="width: 35%">{{ $gr_mst->fc_grno }}</td>
             </tr>
             <tr>
                 <td style="width: 30%;"><b>Dikirim Oleh</b></td>
                 <td style="width: 10%; text-align:center">:</td>
-                <td style="width: 45%">{{ $gr_mst->supplier->fc_supplierlegalstatus }} {{ $gr_mst->supplier->fc_suppliername1 }}</td>
+                <td style="width: 35%">{{ $gr_mst->supplier->fc_supplierlegalstatus }} {{ $gr_mst->supplier->fc_suppliername1 }}</td>
             </tr>
             <tr>
                 <td style="width: 30%;"><b>Jumlah Koli</b></td>
                 <td style="width: 10%; text-align:center" >:</td>
-                <td style="width: 45%">{{ $gr_mst->fn_qtyitem }}</td>
+                <td style="width: 35%">{{ $gr_mst->fn_qtyitem }}</td>
             </tr>
         </table>
         <p style="text-align: center; font-weight:bold; font-size:64px; margin:5px;">{{ $i }}/{{ $count }}</p>
