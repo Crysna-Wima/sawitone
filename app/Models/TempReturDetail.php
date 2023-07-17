76b773834a156018e3116e97611203f5e4e63386
aladino24 <aladinozulmar@gmail.com>
2023-07-17 22:51:29 +0700

    model return master dan retur detail

---
+++ app/Models/TempReturDetail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Builder;
+use App\Blameable;
+use App\Traits\CompositeKey;
+
+class TempRetuDetail extends Model
+{
+    use HasFactory, Blameable, LogsActivity, CompositeKey;
+
+    protected static $logAttributes = ["*"];
+
+    protected $table = 't_tempreturdtl';
+    protected $primaryKey = 'fc_returno';
+    public $incrementing = false;
+    protected $guarded = ['type'];
+
+   // tempreturmaster
+    public function tempreturmaster(){
+     return $this->belongsTo(TempRetuMaster::class, 'fc_returno', 'fc_returno')->withTrashed();
+     }
+
+}
---
+++ app/Models/TempReturMaster.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Builder;
+use App\Blameable;
+use App\Traits\CompositeKey;
+
+class TempRetuMaster extends Model
+{
+    use HasFactory, Blameable, LogsActivity, CompositeKey;
+
+    protected static $logAttributes = ["*"];
+
+    protected $table = 't_tempreturmst';
+    protected $primaryKey = 'fc_returno';
+    public $incrementing = false;
+    protected $guarded = ['type'];
+
+    // returdtl
+    public function tempreturdtl(){
+     return $this->hasMany(TempRetuDetail::class, 'fc_returno', 'fc_returno')->withTrashed();
+     }
+
+}
