150769e50a4b7166b23d0c00fe4f30e8cc79e3fe
Izzudin Fasya <izzudinfasya@gmail.com>
2023-07-18 10:49:51 +0700

    so dtl, sj

--- app/Http/Controllers/Apps/DeliveryOrderController.php
+++ app/Http/Controllers/Apps/DeliveryOrderController.php
@@ -182,7 +182,7 @@ class DeliveryOrderController extends Controller
     public function datatables(){
         $data = SoMaster::with('customer')
         ->where('fc_sotype', 'Retail')
-        ->where('fc_sostatus', ['F', 'P'])  
+        ->whereIn('fc_sostatus', ['F', 'P'])  
         ->where('fc_branch', auth()->user()->fc_branch)->get();
 
         return DataTables::of($data)
