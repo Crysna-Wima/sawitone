d44c08ef496458aae5687a8bcba0e4582f5bb546
Izzudin Fasya <izzudinfasya@gmail.com>
2023-07-27 22:35:31 +0700

    transaksi index

--- app/Http/Controllers/Apps/TransaksiController.php
+++ app/Http/Controllers/Apps/TransaksiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Apps;
 
 use App\Http\Controllers\Controller;
-
+use App\Models\MappingMaster;
 use Carbon\Carbon;
 use DB;
 use App\Models\NotificationDetail;
@@ -17,6 +17,24 @@ class TransaksiController extends Controller
 
     public function index()
     {
-        return view('apps.pembayaran-pelunasan.index');
+        return view('apps.transaksi.index');
+    }
+
+    public function create()
+    {
+        return view('apps.transaksi.create-index');
+    }
+
+    public function select_mapping($fc_mappingcode)
+    {
+        $fc_mappingcode = base64_decode($fc_mappingcode);
+        $data = MappingMaster::where('fc_branch', auth()->user()->fc_branch)->where('fc_mappingcode', $fc_mappingcode)->first();
+        // retur json
+        return response()->json(
+            [
+                'data' => $data,
+                'status' => 'success'
+            ]
+        );
     }
 }
