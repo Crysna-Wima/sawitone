a711058fdc9ab7af5bd94f7cd2d392795ad7b4f5
aladino24 <aladinozulmar@gmail.com>
2023-07-28 19:35:58 +0700

    get data master coa

--- resources/views/data-master/master-coa/index.blade.php
+++ resources/views/data-master/master-coa/index.blade.php
@@ -259,7 +259,9 @@
                         $('#fc_parentcode').prop('disabled', true);
                         $('#fc_parentcode_hidden').val(data.parent.fc_coacode);
                     }
-                    $('#fc_group').append(`<option value="${data.fc_group}" selected>${data.transaksitype.fv_description}</option>`);
+                    if(data.transaksitype != null){
+                        $('#fc_group').append(`<option value="${data.fc_group}" selected>${data.transaksitype.fv_description}</option>`);
+                    }
                     $('#fv_description').val(data.fv_description);
                 } else {
                     iziToast.error({
