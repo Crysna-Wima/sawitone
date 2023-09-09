a84bbd1334817848413f79ced59c0bc56387f8ea
Izzudin Fasya <izzudinfasya@gmail.com>
2023-09-09 19:42:38 +0700

    inv

--- resources/views/apps/invoice-pembelian/create.blade.php
+++ resources/views/apps/invoice-pembelian/create.blade.php
@@ -343,7 +343,7 @@
                         <div class="flex-row-item" style="margin-right: 30px">
                             <div class="d-flex" style="gap: 5px; white-space: pre">
                                 <p class="text-secondary flex-row-item" style="font-size: medium">Item</p>
-                                <p class="text-success flex-row-item text-right" style="font-size: medium" id="fn_dodetail">0,00</p>
+                                <p class="text-success flex-row-item text-right" style="font-size: medium" id="count_item">0,00</p>
                             </div>
                             <div class="d-flex">
                                 <p class="flex-row-item"></p>
@@ -359,12 +359,12 @@
                             </div>
                             <div class="d-flex" style="gap: 5px; white-space: pre">
                                 <p class="text-secondary flex-row-item" style="font-size: medium">Total</p>
-                                <p class="text-success flex-row-item text-right" style="font-size: medium" id="fm_netto">0,00</p>
+                                <p class="text-success flex-row-item text-right" style="font-size: medium" id="total_harga">0,00</p>
                             </div>
                         </div>
                         <div class="flex-row-item">
                             <div class="d-flex" style="gap: 5px; white-space: pre">
-                                <p class="text-secondary flex-row-item" style="font-size: medium">Pelayanan</p>
+                                <p class="text-secondary flex-row-item" style="font-size: medium">Lain-lain</p>
                                 <p class="text-success flex-row-item text-right" style="font-size: medium" id="fm_servpay_calculate">0,00</p>
                             </div>
                             <div class="d-flex">
@@ -381,7 +381,7 @@
                             </div>
                             <div class="d-flex" style="gap: 5px; white-space: pre">
                                 <p class="text-secondary flex-row-item" style="font-weight: bold; font-size: medium">GRAND</p>
-                                <p class="text-success flex-row-item text-right" style="font-weight: bold; font-size:medium" id="fm_brutto">Rp. 0,00</p>
+                                <p class="text-success flex-row-item text-right" style="font-weight: bold; font-size:medium" id="grand_total">Rp. 0,00</p>
                             </div>
                         </div>
                     </div>
@@ -562,6 +562,8 @@
                 $("#fm_disctotal").trigger("change");
                 $('#count_item').html(data[0].tempinvmst.fn_invdetail);
                 $("#count_item").trigger("change");
+
+                console.log(data);
             }
         }
     });
