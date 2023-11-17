55879b2c2fd816db1ce5ba9e23e7bbb3dab7e03b
iostream-code <aryakmifjannah2603@gmail.com>
2023-11-17 23:43:52 +0700

    modify: TempDoMaster model

--- app/Models/TempDoMaster.php
+++ app/Models/TempDoMaster.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Blameable;
+use App\Traits\CompositeKey;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TempDoMaster extends Model
 {
-    use HasFactory;
+    use HasFactory, Blameable, LogsActivity, CompositeKey;
+
+    protected static $logAttributes = ["*"];
+
+    protected $table = 't_tempdomst';
+    protected $primaryKey = 'fc_dono';
+    public $incrementing = false;
+    protected $guarded = ['type'];
+    public $timestamps = true;
+
+    public function somst()
+    {
+        return $this->belongsTo(SoMaster::class, 'fc_sono', 'fc_sono');
+    }
+
+    public function dodtl()
+    {
+        return $this->hasMany(TempDoDetail::class, 'fc_dono', 'fc_dono');
+    }
 }
